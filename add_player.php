11450ca1c5d48e60513814b46c696f07903a66c1
John Migliorisi <johnmigliorisi@gmail.com>
2016-09-13 21:08:07 -0400

    player add form edits

--- add_player.php
+++ add_player.php
@@ -16,16 +16,17 @@ if ($dbh->getInstance() === null) {
 
 if (isset($_POST['submit'])) {
     // Process the form
-	$leaguename = $_POST["leaguename"];
-    $num_teams = $_POST["num_teams"];
-    $tournamentid = $_POST["tournamentid"];
+	$playerName = $_POST["playerName"];
+    $playerTag = $_POST["playerTag"];
+    $bio = $_POST["bio"];
+    $ranking = $_POST["ranking"];
 
     //attempt to add the tournament
-    $addLeague = insert_player($dbh, $leaguename, $num_teams, $tournamentid);
-    if(!$addLeague){
-        echo "failed to create league...";
+    $addPlayer = insert_player($dbh, );
+    if(!$addPlayer){
+        echo "failed to add Player...";
     } else {
-        echo $leaguename . " created!<br />";
+        echo $playerName . " created!<br />";
     }
     
 }  // end: if (isset($_POST['submit']))
@@ -34,40 +35,35 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html>
 <head>
- <!-- Bootstrap -->
+    <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css"  href="bootstrap/css/bootstrap.css">
-
 
     <!-- custom styles -->
     <link rel="stylesheet" type="text/css" href="css/custom_styles.css">
-
 
 <title>add player</title>
 </head>
 
 <body>
     <div class="container">  
-    <h2>Use this form to create a league</h2>  
+    <h2>Use this form to add a player</h2>  
         <div id="loginbox" style="margin-top:50px;" class="col-md-4 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <form method="post" id="leagueform" class="form-horizontal" data-toggle="validator" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <form method="post" id="addplayer" class="form-horizontal" data-toggle="validator" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             
-                <label for="leaguename">League Name</label>           
-                    <input type="text" class="form-control" name="leaguename" value="" placeholder="" required>  
+                <label for="playerName">Player Name</label>           
+                    <input type="text" class="form-control" name="playerName" value="" placeholder="" required>  
             
-                <label for="num_teams">Number of Teams</label>           
-                    <input type="text" class="form-control" name="num_teams" value="" placeholder="" required>  
+                <label for="playerTag">Player Tag</label>           
+                    <input type="text" class="form-control" name="playerTag" value="" placeholder="" required>  
 
-                <label for="tournamentid">Select a Tournament</label>
-                    <select class="form-control" name="tournamentid" required>
-                    <option value="" selected>Choose a tournament</option>
-                    <?php foreach ($results as $row) { ?>
+                <label for="bio">Bio</label>
+                    <textarea name="bio" class="form-control" value="" placeholder=""></textarea>
 
-                        <option value="<?php echo $row['id']; ?>"><?php echo $row['tournament_name']; ?></option>"
-                    <?php } ?>
-                    </select>
+                <label for="rank">Player Rank</label>           
+                    <input type="text" class="form-control" name="rank" value="" placeholder="" required> 
                     
                                      
-                <button type="submit" class="btn btn-success" name="submit" value="createleague">Create League</button>
+                <button type="submit" class="btn btn-success" name="submit" value="addplayer">Add Player</button>
             </form>     
         </div>
     </div>
